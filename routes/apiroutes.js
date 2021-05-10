e316a594015f007d0ee2e05ba4cb5e6f3ef42592
VikingFrog <eellis4500@gmail.com>
2021-05-10 18:17:25 -0400

    finished routes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,8 +1,56 @@
-var express = require('express')
-var router = express.Router()
+var router = require('express').Router()
+var Workout = require("../models/workout")
 
-router.put('/api', (req, res) => {
-    const routWorkout = JSON.parse(fs.readFileSync(path.join))
+router.post('/api/workouts', (req, res) => {
+    Workout.create({})
+    .then(( dbworkouts ) => {
+        res.json(dbworkouts)
+    })
+    .catch(err => {
+        res.json(err);
+    })
+})
+
+router.get('/api/workouts/range', (req, res) => {
+    Workout.aggregate([{
+        $addfields:{
+            totalDuration:{
+                $sum:"$exercises.duration"
+            }
+        }
+    }])
+    .sort({_id:-1})
+    .then(( dbworkouts ) => {
+        console.log(dbworkouts)
+        res.json(dbworkouts)
+    })
+    .catch(err => {
+        res.json(err);
+    })
+})
+
+router.put('/api/workouts/:id', ({body, params}, res) => {
+    Workout.findByIdAndUpdate(
+        params.id,
+        { $push: { exercises: body }},
+        { new: true })
+        .then(( dbworkouts ) => {
+            res.json(dbworkouts)
+        })
+        .catch(err => {
+            res.json(err);
+        })
+})
+
+router.get('/api/workouts', (req, res) => {
+    Workout.find()
+    .then(( dbworkouts ) => {
+        console.log(dbworkouts)
+        res.json(dbworkouts)
+    })
+    .catch(err => {
+        res.json(err);
+    })
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
-
-const apiRoutes = require('./routes/api');
-const htmlRoutes = require('./routes/html');
+const mongoose = require("mongoose");
+const router = require('./routes/apiroutes');
 
 const app = express();
 
@@ -11,8 +10,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"))
 
-app.use("/api", apiRoutes)
-app.use("/", htmlRoutes)
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+app.use(require("./routes/apiroutes"));
+app.use(require("./routes/htmlroutes"));
 
 app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`);
